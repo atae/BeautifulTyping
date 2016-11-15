77b090be6ab2c69f146f3857c8d00e78ccb4b96f
Andrew Tae <andrewtae3368@gmail.com>
2016-11-14 22:20:51 -0800

    Implement Audio

--- assets/entry.jsx
+++ assets/entry.jsx
@@ -2,12 +2,19 @@ import anime from "animejs"
 import {startLevel, handleKeyboard} from './javascript/game';
 
 document.addEventListener("DOMContentLoaded", () => {
-  let currentLevel = ["Let's get this party started!", "Whoa, two sentences!", "THREEEEEE;;;;;", "end"]
+  let currentLevel = {
+    level: "Tutorial",
+    currentText: ["Let's get this party started!", "Whoa, two sentences!", "THREEEEEE;;;;;", "end"],
+    soundFiles: './assets/music/Beautiful_Typing.mp3',
+    sfx: ['./assets/sounds/Blip_Select.wav']
+  }
 
+  //ToggleSound
   let textToType = ["Let's get this typed.", "Let's also get this typed"]
   document.addEventListener("keydown", (e) => {
     if (e.key == "1") {
     startLevel(currentLevel);
+
     }
   })
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,35 @@
-
+import Howler from 'howler'
 
 // implement new Date once I figure it out
   export const startLevel = (currentLevel) => {
-    console.log(currentLevel);
+    let currentText = currentLevel['currentText']
+
+    console.log(currentText);
     let keys_entered = 0;
     let wpm;
     let time = 0;
     let maxWpm = 0;
     let errors = 0;
-    $('.currentText').replaceWith(`<h2 class="currentText">${currentLevel[0]}</h2>`)
+    let done = ""
+    $('.currentText').replaceWith(`<h2 class="currentText">${currentText[0]}</h2>`)
+    $('.done').replaceWith(`<h3 class="done">${done}</h3>`)
+
+    let soundFiles = currentLevel['soundFiles']
+    let playMusic = new Howler.Howl({
+      src: [soundFiles]
+    });
+
+    let sfx = currentLevel['sfx']
+    let sfx1 = new Howler.Howl({
+      src: [sfx[0]],
+      volume: 0.4
+    });
+
+    playMusic.play();
+    $('.Level').replaceWith(`<li class="Level"> Level: ${currentLevel['level']} </li>`)
+    $('.Timer').replaceWith(`<li class="Timer">Time: ${time} seconds`)
+    $('.wpm').replaceWith(`<li class="wpm">WPM: ${wpm} wpm </li>`)
+    $('.maxWpm').replaceWith(`<li class="maxWpm">Max WPM: ${maxWpm} wpm</li>`)
 
     let gameWatcher = setInterval(() => {
       time++;
@@ -17,44 +38,52 @@
     if (wpm > maxWpm) {
       maxWpm = wpm
     }
+      $('.Level').replaceWith(`<li class="Level"> Level: ${currentLevel['level']} </li>`)
       $('.Timer').replaceWith(`<li class="Timer">Time: ${time} seconds`)
       $('.wpm').replaceWith(`<li class="wpm">WPM: ${wpm} wpm </li>`)
       $('.maxWpm').replaceWith(`<li class="maxWpm">Max WPM: ${maxWpm} wpm</li>`)
     },
      1000)
+     $('.navbar').toggleClass('hidden')
 
-     let done = ""
     document.addEventListener('keydown', (e) => {
-      if (currentLevel.length > 1) {
-        if (e.key === currentLevel[0][0]){
+      if (currentText.length > 1) {
+        if (e.key === currentText[0][0]){
           // debugger
-          done += currentLevel[0][0]
-          currentLevel[0] = currentLevel[0].slice(1)
-          console.log(currentLevel);
+          done += currentText[0][0]
+          currentText[0] = currentText[0].slice(1)
+          console.log(currentText);
           keys_entered++
           $('.done').replaceWith(`<h3 class="done">${done}</h3>`)
-          $('.currentText').replaceWith(`<h2 class="currentText">${currentLevel[0]}</h2>`)
+          if (currentText[0][0] == " " || done[done.length-1] == " "){
+            $('.currentText').replaceWith(`<h2 class="currentText">\u00A0${currentText[0]}</h2>`)
+            //Come back to this space glitch later
+          } else {
+            $('.currentText').replaceWith(`<h2 class="currentText">${currentText[0]}</h2>`)
 
+          }
         } else if(e.key !== "Shift"){
           errors ++
+          sfx1.play();
           $(`.errors`).replaceWith(`<li class="errors"> Errors: ${errors}</li>`)
         }
-          if (currentLevel[0] == "" && currentLevel[1] == "end") {
-            currentLevel = currentLevel.slice(1)
+          if (currentText[0] == "" && currentText[1] == "end") {
+            playMusic.stop();
+            currentText = currentText.slice(1)
             clearInterval(gameWatcher);
             done = ""
             $('.done').replaceWith(`<h3 class="done">${done}</h3>`)
             $(`.currentText`).replaceWith(`<h2> That's it! </h2>`)
             //replace this line with results screen in the future
             document.removeEventListener('keydown',(e) => { handleKeyboard(e)})
-          } else if (currentLevel[0].length === 0) {
-            currentLevel = currentLevel.slice(1)
+          } else if (currentText[0].length === 0) {
+            currentText = currentText.slice(1)
             done = ""
             $('.done').replaceWith(`<h3 class="done">${done}</h3>`)
-            $('.currentText').replaceWith(`<h2 class="currentText">${currentLevel[0]}</h2>`)
+            $('.currentText').replaceWith(`<h2 class="currentText">${currentText[0]}</h2>`)
           }
 
-      } else if (currentLevel[0] == "end"){
+      } else if (currentText[0] == "end"){
 
       }
           // $('.keys-entered').replaceWith(`<li class="keys-entered">Correct Keys Entered: ${keys_entered} </li>`)
@@ -65,6 +94,9 @@
 
 
 
+}
+
+  export const playMusic= (currentLevel) => {
 
   }
   //
